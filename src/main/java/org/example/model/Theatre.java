f8bcfd82db0db70127d2c8b9c628eb233ec27f3f
bauRoccella <broccella@uade.edu.ar>
2025-04-13 16:15:41 -0300

    theatre update

--- src/main/java/org/example/model/Theatre.java
+++ src/main/java/org/example/model/Theatre.java
@@ -1,14 +1,14 @@
 package org.example.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Theatre {
     private static Theatre instance;
     private final Room[] rooms;
 
     private Theatre(int numberOfRooms) {
         this.rooms = new Room[numberOfRooms];
+        for (int i = 0; i < numberOfRooms; i++) {
+            rooms[i] = new Room(i, 210); // suponiendo que todas las salas tienen la misma capacidad
+        }
     }
 
     public static Theatre getInstance(int numberOfRooms) {
@@ -21,4 +21,14 @@ public class Theatre {
     public Room[] getRooms() {
         return rooms;
     }
+
+    public void addFunctionToRoom(int roomNumber, Function function) {
+        if (roomNumber >= 1 && roomNumber <= rooms.length) {
+            Room room = rooms[roomNumber - 1];
+            room.addFunction(function);
+        } else {
+            System.out.println("Número de sala inválido: " + roomNumber);
+        }
+    }
+
 }
